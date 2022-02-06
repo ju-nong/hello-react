c966ea44ac605d6a2e4fef08475675735bd6a6a8
JunYong Lee <iiiuooyoo@gmail.com>
2022-02-06 22:30:08 +0900

    1일차

---
+++ src/MyComponent.js
@@ -0,0 +1,31 @@
+// function MyComponet() {
+//   return <div>나의 새롭고 멋진 컴포넌트</div>;
+// }
+
+// const MyComponent = () => <div>나의 새롭고 멋진 컴포넌트</div>;
+
+import PropTypes from "prop-types";
+
+const MyComponent = ({ name, favoriteNumber, children }) => {
+  return (
+    <div>
+      안녕하세요, 제 이름은 {name}입니다.<br></br>
+      children 값은 {children}
+      입니다.
+      <br />
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
+    </div>
+  );
+};
+
+MyComponent.defaultProps = {
+  name: "기본 이름",
+  children: "공백",
+};
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
+};
+
+export default MyComponent;
